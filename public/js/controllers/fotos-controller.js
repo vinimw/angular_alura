0ee95f4bd4e10ae0a2c357b440bde17790fe2f87
vinimw <vini_88mw@hotmail.com>
2017-08-30 00:34:03 -0300

    full project first lesson

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,8 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+	$scope.foto = {
+		titulo : 'teste',
+		src : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+		
+	};
+});
---
+++ public/js/controllers/photo-controller.js
@@ -0,0 +1,8 @@
+angular.module('alurapic').controller('PhotoController', function($scope) {
+	
+	$scope.photo = {
+		title = 'teste',
+		src = 'http://www.fundosanimais.com/Minis/leoes.jpg'
+		
+	};
+});
