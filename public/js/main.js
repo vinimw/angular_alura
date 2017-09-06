89f80b1efea9814562049fa5282d86bb6db53e49
vinimw <vini_88mw@hotmail.com>
2017-09-06 01:38:17 -0300

    class 4 - adding routes and configure to html5mode provider

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
-angular.module('alurapic', ['myDirectives','ngAnimate']);
+angular.module('alurapic', ['myDirectives','ngAnimate','ngRoute'])
+.config(function($routeProvider, $locationProvider) {
+
+	$locationProvider.html5Mode(true);
+
+	$routeProvider.when('/fotos', {
+		templateUrl: 'partials/home.html',
+		controller: 'FotosController'
+	});
+
+	$routeProvider.otherwise({
+		redirectTo: '/fotos'
+	});
+
+});
